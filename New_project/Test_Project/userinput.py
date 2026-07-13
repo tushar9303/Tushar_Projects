63fa18307541e5ae126f4ad06e7e19a1d1e15c9b
Tushar9303 <Tushar.sharma9303@gmail.com>
2026-07-14 00:05:48 +0530

    userinput

--- New_project/Test_Project/userinput.py
+++ New_project/Test_Project/userinput.py
@@ -1,2 +1,2 @@
 name = input("Please enter your name: ")
-print("Hello " + name + " this is a simple python program")
+print("Hello " + name + " this is a simple python program that takes user input and prints a greeting message.")
